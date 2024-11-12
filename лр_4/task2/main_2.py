ca84b1e99b61a6a0252e489eeac51081cfcbb749
artem961 <artemkhrabrov03@gmail.com>
2024-11-12 23:26:19 +0300

    Start

---
+++ лр_4/task2/main_2.py
@@ -0,0 +1,16 @@
+import json
+import yaml
+
+file = open("../timetable.json", "r", encoding="utf-8")
+text = json.load(file)  # В переменную загрузятся данные из json файла
+file.close()
+
+# Конвертация и запись
+rezult = yaml.dump(text, allow_unicode=True)
+out = open("timetable_2.yaml", "w", encoding="utf-8")
+out.write(rezult)
+out.close()
+
+
+
+
---
+++ лр_4/task3/main_3.py
@@ -0,0 +1,11 @@
+from json_to_dict import json_to_dict
+from dict_to_yaml import dict_to_yaml
+
+rezult = dict_to_yaml(json_to_dict("../timetable.json"))
+out = open("timetable_1.yaml", "w", encoding="utf-8")
+out.write(rezult)
+out.close()
+
+
+
+
